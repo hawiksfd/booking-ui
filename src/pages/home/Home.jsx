411cadbf0167fe15d673f8adb21929b00f6975f4
hawiksfd <hawiksyaef@gmail.com>
2022-06-10 15:56:48 +0700

    Menambah mail list subscribe

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Featured from "../../components/featured/Featured";
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Header from "../../components/header/Header";
+import MailList from "../../components/mailList/MailList";
 import Navbar from "../../components/navbar/Navbar";
 import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
@@ -21,6 +22,7 @@ const Home = () => {
 
                 <h1 className="homeTitle">Homes guests love</h1>
                 <FeaturedProperties />
+                <MailList />
 
             </div>
         </div>
